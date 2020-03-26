d67504c55f8c2bc64eb4ab1b5c641b1121588798
Luke <luke.sexton@my.jcu.edu.au>
2020-03-26 13:25:06 +1000

    Create boilerplate code.

---
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/CalculateActivity.java
@@ -0,0 +1,13 @@
+package au.edu.jcu.cp3406.bmicalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+    }
+}
