5df359b5b50e1c5c7e8c01183d67f0467aea7ede
Luke <luke.sexton@my.jcu.edu.au>
2020-03-26 13:54:03 +1000

    Add boilerplate code for calculate clicked function.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
@@ -1,7 +1,9 @@
 package au.edu.jcu.cp3406.bmicalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ImperialActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class ImperialActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imperial);
+    }
+
+    public void calculateClicked(View view) {
+        // Send intent to start Calculate activity.
+        Intent intent = new Intent(this, CalculateActivity.class);
+        startActivity(intent);
     }
 }
