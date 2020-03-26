def33d2a52e4f1a020885f4c267215e384ae52e1
Luke <luke.sexton@my.jcu.edu.au>
2020-03-26 15:34:02 +1000

    Add override methods for creating main menu and function for clicking the settings icon.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
@@ -3,9 +3,21 @@ package au.edu.jcu.cp3406.bmicalculator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import androidx.annotation.NonNull;
+
 
 public class ImperialActivity extends Activity {
+    private static final String FEET_KEY = "feet";
+    private static final String INCHES_KEY = "inches";
+    private static final String WEIGHT_KEY = "weight";
+    private int feet;
+    private int inches;
+    private int weight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +25,31 @@ public class ImperialActivity extends Activity {
         setContentView(R.layout.activity_imperial);
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        // Save user values to instance state.
+        super.onSaveInstanceState(outState);
+        outState.putInt(FEET_KEY, feet);
+        outState.putInt(INCHES_KEY, inches);
+        outState.putInt(WEIGHT_KEY, weight);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
     public void calculateClicked(View view) {
         // Send intent to start Calculate activity.
         Intent intent = new Intent(this, CalculateActivity.class);
         startActivity(intent);
+    }
+
+    public void settingsClicked(MenuItem menuItem) {
+        // Create intent to start the settings activity that expects a result.
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, SettingsActivity.SETTINGS_REQUEST);
     }
 }
