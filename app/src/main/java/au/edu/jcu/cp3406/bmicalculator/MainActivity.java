e6b50cb9d608e277d220468bdc2fee3b9ebc8d14
Luke <luke.sexton@my.jcu.edu.au>
2020-03-27 17:54:20 +1000

    Add intent to display welcome page for first time user.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PREFERENCES_KEY = "preferences";
     private static final String MEASUREMENT_KEY = "measurement";
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,9 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
         boolean isFirstRun = preferences.getBoolean("is_first_run", false);
 
-        Intent intent;
         if (!isFirstRun) {
             setContentView(R.layout.activity_main);
-            intent = new Intent(this, SettingsActivity.class);
+
         } else {
             boolean measurement = preferences.getBoolean(MEASUREMENT_KEY, false);
 
@@ -33,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void settingsClicked() {
-
+    public void settingsClicked(View view) {
+        intent = new Intent(this, SettingsActivity.class);
     }
 
 }
