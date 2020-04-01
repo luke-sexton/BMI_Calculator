5578bc9bd6958eb0036e4be28b2ab30e6dd3f742
Luke <luke.sexton@my.jcu.edu.au>
2020-04-01 14:47:48 +1000

    Create calculator class with boilerplate code and function templates.

---
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/Calculator.java
@@ -0,0 +1,15 @@
+package au.edu.jcu.cp3406.bmicalculator;
+
+public class Calculator {
+    private double calculation;
+
+    public Calculator() {}
+
+    public double calculateMetric(int height, int weight) {
+        return calculation;
+    }
+
+    public double calculateImperial(int feet, int inches, int weight) {
+        return calculation;
+    }
+}
