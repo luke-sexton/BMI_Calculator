7d032c58c3256c0d6743ba22a82eed293ad80bb0
Luke <luke.sexton@my.jcu.edu.au>
2020-04-01 14:41:27 +1000

    Refactor name.

---
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ResultsActivity.java
@@ -0,0 +1,31 @@
+package au.edu.jcu.cp3406.bmicalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Create options menu for settings icon.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void settingsIconClicked(MenuItem item) {
+        // Create intent to start the settings activity that expects a result.
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, SettingsActivity.SETTINGS_REQUEST);
+    }
+}
