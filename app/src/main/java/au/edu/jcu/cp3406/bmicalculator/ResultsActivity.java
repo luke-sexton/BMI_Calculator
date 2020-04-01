22e59df22fc0bc47e1007aed719d5f7269595087
Luke <luke.sexton@my.jcu.edu.au>
2020-04-01 20:49:49 +1000

    Set results text view with BMI calculation.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ResultsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ResultsActivity.java
@@ -2,30 +2,33 @@ package au.edu.jcu.cp3406.bmicalculator;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultsActivity extends AppCompatActivity {
+    private static final String CALCULATION_KEY = "Calculation";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set content and text views with reference id.
         setContentView(R.layout.activity_results);
+        TextView resultsView = findViewById(R.id.results);
+
+        // Get user's information from intent.
+        Intent intent = getIntent();
+        double calculatedBMI = intent.getDoubleExtra(CALCULATION_KEY, 0.0);
+
+        // Display user's BMI on results TextView.
+        String resultText = "Your BMI Results: " + calculatedBMI;
+        resultsView.setText(resultText);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Create options menu for settings icon.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public void settingsIconClicked(MenuItem item) {
-        // Create intent to start the settings activity that expects a result.
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivityForResult(intent, SettingsActivity.SETTINGS_REQUEST);
+    public void okayClicked(View view) {
+        // Finish and return to previous activity.
+        finish();
     }
 }
