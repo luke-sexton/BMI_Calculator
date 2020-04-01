9af711f52ccbc1643afb181de56957ac9a178aee
Luke <luke.sexton@my.jcu.edu.au>
2020-04-01 15:14:14 +1000

    Create BMI calculation functions.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/Calculator.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/Calculator.java
@@ -1,15 +1,31 @@
 package au.edu.jcu.cp3406.bmicalculator;
 
 public class Calculator {
+    private static final int FEET_TO_INCHES = 12;
+    private static final int CENTIMETRES_TO_METRES = 100;
+
     private double calculation;
 
     public Calculator() {}
 
     public double calculateMetric(int height, int weight) {
-        return calculation;
+        // Metric BMI Formula = weight (kg) / [height (m)]^2
+
+        // Convert height to metres.
+        height /= CENTIMETRES_TO_METRES;
+
+        // Return metric BMI formula calculation.
+        return calculation = weight / Math.pow(height, 2);
     }
 
     public double calculateImperial(int feet, int inches, int weight) {
-        return calculation;
+        // Imperial BMI Formula = 703 x weight (lb) / [height (in)]^2
+
+        // Convert feet to inches
+        feet *= FEET_TO_INCHES;
+        double total_height = feet + inches;
+
+        // Return imperial BMI formula calculation
+        return calculation = 703 * (weight / Math.pow(total_height, 2));
     }
 }
