8e0cd12770640adbbefaad58d7ab200375305346
Luke <luke.sexton@my.jcu.edu.au>
2020-04-02 16:17:17 +1000

    Clean up code.

--- app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/bmicalculator/ImperialActivity.java
@@ -129,10 +129,12 @@ public class ImperialActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         };
     }
 
